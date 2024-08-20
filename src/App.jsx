ae59456da39755b7de317f2366c8d4e616cc08cc
vishwaselvakumar <vishwaselvakumar2003@gmail.com>
2024-08-20 09:51:26 +0530

    vish

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Home from './pages/Home';
 import Register from './pages/Register';
+import { Categories } from './pages/Categories';
+import { C1 } from './pages/C1';
+import { C2 } from './pages/C2';
+import { C3 } from './pages/C3';
+import { C4 } from './pages/C4';
+import { C5 } from './pages/C5';
+import { C6 } from './pages/C6';
+import { C7 } from './pages/C7';
+import { C8 } from './pages/C8';
 function App() {
   const user = true
 
@@ -12,6 +21,15 @@ function App() {
         <Route path="/" element={user?<Home />:<Login/>} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path='/categories' element={<Categories/>} />
+        <Route path='/c1' element={<C1/>} />
+        <Route path='/c2' element={<C2/>} />
+        <Route path='/c3' element={<C3/>} />
+        <Route path='/c4' element={<C4/>} />
+        <Route path='/c5' element={<C5/>} />
+        <Route path='/c6' element={<C6/>} />
+        <Route path='/c7' element={<C7/>} />
+        <Route path='/c8' element={<C8/>} />
       </Routes>
     </Router>
     </>
